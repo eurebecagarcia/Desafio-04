add250dd1545684ce39edf954e9a34f7a41486b8
eu.rebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-06-15 20:45:02 -0300

    challenge 04

---
+++ script.js
@@ -0,0 +1,32 @@
+function mensagem(msg){
+    return console.log(msg);
+}
+
+mensagem('Nada é tão ruim que não possa piorar')
+
+
+function nome(name){
+    return console.log(name)
+}
+nome('Rebeca')
+
+
+function mostrarDados(nome, idade, musica){
+    return console.log(`meu nome é ${nome},tenho ${idade} anos e curto musica ${musica}`)
+}
+mostrarDados('Rebeca', 26, 'pop')
+
+
+function filmeMusica(filme, musica){
+    return console.log(filme, musica)
+}
+filmeMusica('O Rei do show', 'This Is Me')
+
+
+function triplicar(number){
+    return number*3
+}
+let resultado= triplicar(10)
+console.log(resultado)
+
+
